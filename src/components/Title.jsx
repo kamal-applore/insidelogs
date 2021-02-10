17e315485765625570cb3d80b0ac56df2d964a8c
Kamal Shah <kkamal714@gmail.com>
2021-02-10 13:27:41 +0530

    [Kamal] | Adds WhyUs Section

---
+++ src/components/Title.jsx
@@ -0,0 +1,41 @@
+import { Box, makeStyles } from '@material-ui/core'
+import React from 'react'
+import CustomHeading from './CustomHeading'
+
+const useStyles = makeStyles((theme) => ({
+  divider: {
+    borderTop: '0.5px solid #000000',
+    width: '77%',
+    marginLeft: '10px',
+    marginTop: '15px',
+    [theme.breakpoints.up('lg')]: {
+      marginTop: '20px',
+    },
+  },
+  heading: {
+    fontWeight: theme.typography.fontWeightMedium,
+    [theme.breakpoints.up('lg')]: {
+      fontSize: '32px',
+      lineHeight: '38px',
+    },
+  },
+  heading2: {
+    color: theme.palette.primary.main,
+    marginLeft: '5px',
+
+    [theme.breakpoints.up('md')]: {
+      marginLeft: '10px',
+    },
+  },
+}))
+
+export default function Title({ heading, heading2 }) {
+  const classes = useStyles()
+  return (
+    <Box display="flex">
+      <CustomHeading heading={heading} className={classes.heading} />
+      <CustomHeading heading={heading2} className={`${classes.heading} ${classes.heading2}`} />
+      <Box className={classes.divider} />
+    </Box>
+  )
+}
---
+++ src/sections/WhyUs.js
@@ -0,0 +1,81 @@
+import { Grid, makeStyles } from '@material-ui/core'
+import React from 'react'
+import ContentLayout from '../components/ContentLayout'
+import CustomButton from '../components/CustomButton'
+import CustomHeading from '../components/CustomHeading'
+import CustomLabel from '../components/CustomLabel'
+import Title from '../components/Title'
+import Icon1 from '../assets/images/icon1.png'
+import Icon2 from '../assets/images/icon2.png'
+import Icon3 from '../assets/images/icon3.png'
+import Icon4 from '../assets/images/icon4.png'
+import CustomImage from '../components/CustomImage'
+
+const useStyles = makeStyles((theme) => ({
+  content: {
+    marginTop: '30px',
+    [theme.breakpoints.up('md')]: { marginTop: '50px' },
+    [theme.breakpoints.up('lg')]: { marginTop: '85px' },
+  },
+
+  label: {
+    textAlign: 'center',
+    margin: '20px 0px',
+
+    [theme.breakpoints.up('md')]: {
+      marginTop: '15px',
+      marginBottom: '80px',
+    },
+    [theme.breakpoints.up('lg')]: {
+      marginTop: '24px',
+      marginBottom: '104px',
+    },
+  },
+  image: {
+    width: '80px',
+    display: 'flex',
+    margin: '0px auto',
+  },
+}))
+
+export default function WhyUs() {
+  const classes = useStyles()
+  const whyUs = [
+    {
+      icon: Icon1,
+      label: 'Expert Work',
+    },
+    {
+      icon: Icon2,
+      label: '24/7 Support',
+    },
+    {
+      icon: Icon3,
+      label: 'Creative Design',
+    },
+    {
+      icon: Icon4,
+      label: 'Well Experienced',
+    },
+  ]
+  return (
+    <div>
+      <ContentLayout
+        heading="Inside Logs have expert workers who
+        withstand with your work and create an 
+        well experience for your buisness"
+        title1="WHY"
+        title2="US"
+      >
+        <Grid container className={classes.content}>
+          {whyUs.map((item, index) => (
+            <Grid item xs={12} md={6} key={index}>
+              <CustomImage image={item.icon} className={classes.image} />
+              <CustomLabel label={item.label} className={classes.label} />
+            </Grid>
+          ))}
+        </Grid>
+      </ContentLayout>
+    </div>
+  )
+}
