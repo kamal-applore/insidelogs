8a933406fc1f859ff7d88e86c6d467c62e1fd66a
Kamal Shah <kkamal714@gmail.com>
2021-02-10 13:28:54 +0530

    [Kamal] | Adds Section to Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,62 @@
 /* eslint-disable import/no-anonymous-default-export */
+import { makeStyles } from '@material-ui/core'
 import React from 'react'
 import Layout from '../components/Layout'
+import CTABanner from '../sections/CTABanner'
+import HeroSection from '../sections/HeroSection'
+import OurServices from '../sections/OurServices'
+import WhyUs from '../sections/WhyUs'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    [theme.breakpoints.up('md')]: {
+      paddingLeft: '80px',
+    },
+    [theme.breakpoints.up('lg')]: {
+      paddingLeft: '130px',
+    },
+  },
+  heroSection: {
+    marginTop: '30px',
+    padding: '0px 10px',
+    [theme.breakpoints.up('md')]: {
+      marginTop: '50px',
+      padding: 0,
+    },
+    [theme.breakpoints.up('lg')]: {
+      marginTop: '107px',
+    },
+  },
+  ourServices: {
+    marginTop: '50px',
+    padding: '0px 10px',
+    [theme.breakpoints.up('md')]: {
+      marginTop: '50px',
+      padding: 0,
+    },
+    [theme.breakpoints.up('lg')]: {
+      marginTop: '107px',
+    },
+  },
+}))
 
 export default () => {
+  const classes = useStyles()
   return (
     <Layout>
-      <h1> HOME </h1>
+      <div className={classes.root}>
+        <div className={classes.heroSection}>
+          <HeroSection />
+        </div>
+        <div className={classes.ourServices}>
+          <OurServices />
+        </div>
+        <div className={classes.ourServices}>
+          <WhyUs />
+        </div>
+      </div>
+      {/* < */}
+      <CTABanner />
     </Layout>
   )
 }
